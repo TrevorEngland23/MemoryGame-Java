bd2c3841aadb87b5dfc721c94b4f722b3bcc6cab
TrevorEngland <trevor.england@stmartin.edu>
2023-11-29 13:14:16 -0800

    Main method and Game class v1

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MemoryGame game = new MemoryGame();
+    }
+}
---
+++ MemoryGame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class MemoryGame extends JFrame {
+    
+
+    // ctor(s)
+    public MemoryGame() {
+        super("Memory Game");
+        setSize(900,900);
+        setLocation(500,0);
+        setVisible(true);
+}
+    }
+
+
