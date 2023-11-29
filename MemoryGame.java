d160c44017a3d76c50e37ebbda80dff09d4cd772
TrevorEngland <trevor.england@stmartin.edu>
2023-11-29 13:23:42 -0800

    created rows and columns for panel

--- MemoryGame.java
+++ MemoryGame.java
@@ -1,14 +1,32 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class MemoryGame extends JFrame {
+
+    ArrayList<JButton> buttonList =  new ArrayList<JButton>(); // create an arraylist of buttons
+    final int columns = 4;
+    final int rows = 3;
     
 
     // ctor(s)
     public MemoryGame() {
         super("Memory Game");
+
+        // make a grid layout
+        GridLayout gridLayout = new GridLayout(rows, columns); // rows, columns
+
+        for(int i = 0; i < rows * columns; i++){
+            JButton button = new JButton();
+            buttonList.add(button);
+            add(button);
+        }
+
+        setLayout(gridLayout);
         setSize(900,900);
         setLocation(500,0);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // program exits when user closes the application
 }
     }
 
